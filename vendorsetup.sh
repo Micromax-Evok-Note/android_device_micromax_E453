ab5f63833cd4ec22ed6667fd91a34862506c9df4
Sooraj S Prakash <soorajsprakash007@gmail.com>
2020-06-14 21:39:32 +0530

    Added vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+add_lunch_combo cm_E453-user
+add_lunch_combo cm_E453-userdebug
+add_lunch_combo cm_E453-eng
+
+
+# OR we could do this:
+
+#for var in eng user userdebug; do
+#  add_lunch_combo cm_A7010a48-$var
+#done
