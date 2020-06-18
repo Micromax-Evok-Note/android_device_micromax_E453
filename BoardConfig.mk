15a67fdfc89667f1979e41e4f75c80eefd255321
Sooraj S Prakash <soorajsprakash007@gmail.com>
2020-06-18 20:12:18 +0530

    added BoardConfig and few other makefiles

---
+++ BoardConfig.mk
@@ -0,0 +1,93 @@
+#
+# Copyright (C) 2015-2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Device path
+LOCAL_PATH := device/micromax/E453
+
+# Device board elements
+include $(LOCAL_PATH)/PlatformConfig.mk
+include $(LOCAL_PATH)/board/*.mk
+
+# Device vendor board
+-include vendor/micromax/E453/BoardConfigVendor.mk
+
+#######################################################################
+
+# Kernel
+TARGET_KMODULES := true
+COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+
+BOARD_KERNEL_CMDLINE := console=tty0 console=ttyMT0,921600n1 root=/dev/ram vmalloc=496M androidboot.hardware=mt6735 slub_max_order=0 slub_debug=O bootopt=64S3,32N2,64N2 lcm=1-ili9885b_fhd_dsi_vdo_txd_asi fps=6013 vram=29229056 printk.disable_uart=1 bootprof.pl_t=1502 bootprof.lk_t=2749 boot_reason=4 androidboot.serialno=V027751701600002227 androidboot.bootreason=wdt_by_pass_pwk gpt=1 androidboot.selinux=permissive 
+
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_PAGESIZE := 2048
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel#
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x03f88000 --tags_offset 0x0df88000
+
+
+#TODO: 
+# Disable memcpy opt (for audio libraries)
+TARGET_CPU_MEMCPY_OPT_DISABLE := true
+
+#TODO: 
+# EGL
+BOARD_EGL_CFG := $(LOCAL_PATH)/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+
+#TODO: 
+# Block based ota
+# see http://review.cyanogenmod.org/#/c/78849/1/core/Makefile
+BLOCK_BASED_OTA := false
+
+# Flags
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+BOARD_HAS_FLIPPED_SCREEN := true	
+TARGET_BOOTLOADER_BOARD_NAME := mediatek
+TARGET_BOARD_PLATFORM := mt6753
+TARGET_NO_BOOTLOADER := true
+
+# Filesystem
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Partitions
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 25874137088
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4773117952
+BOARD_CACHEIMAGE_PARTITION_SIZE := 419430400
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# init
+TARGET_PROVIDES_INIT_RC := true
+
+# system.prop
+TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
+
+# Vold
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := sys/class/android_usb/android0/f_mass_storage/lun/file
+TARGET_USE_CUSTOM_LUN_FILE_PATH := sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun0/file #mine
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := E453
+
+# Engineer Mode
+PRODUCT_PACKAGES += \
+        EngineerMode
