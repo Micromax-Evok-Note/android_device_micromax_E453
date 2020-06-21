60bbb35fa11c37bb3d4037003678f3bfe5aa2304
Sooraj S Prakash <soorajsprakash007@gmail.com>
2020-06-21 13:53:48 +0530

    updated rootdir n stuffs

--- lineage.mk
+++ lineage.mk
@@ -15,8 +15,7 @@
 #
 
 LOCAL_PATH := device/micromax/E453
-# Enhanced NFC
-# $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
